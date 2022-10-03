115743e74394053a34c53a6aa2736ed6f9858c7f
Julio Briones <Julio_Briones@epam.com>
2022-10-02 20:13:58 -0500

    Up to adding TailwindCSS.

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import list_listings, retrieve_listing, create_listing
+from .views import list_listings, retrieve_listing, create_listing, update_listing, delete_listing
 
 app_name = "listings"
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path("", list_listings, name="list-listings"),
     path("<int:id>", retrieve_listing, name="retrieve-listing"),
     path("create_listing/", create_listing, name="create-listing"),
+    path("<int:id>/edit/", update_listing, name="update-listing"),
+    path("<int:id>/delete/", delete_listing, name="delete-listing"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 from .forms import ListingForm
 from .models import Listing
@@ -30,3 +30,24 @@ def create_listing(request):
             return redirect("listings:list-listings")
     context = {"form": form}
     return render(request, "create_listing.html", context)
+
+
+def update_listing(request, id):
+    listing = get_object_or_404(Listing, id=id)
+    form = ListingForm(instance=listing)
+    if request.method == "POST":
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect("listings:list-listings")
+    context = {
+        "listing": listing,
+        "form": form
+    }
+    return render(request, "update_listing.html", context)
+
+
+def delete_listing(request, id):
+    listing = get_object_or_404(Listing, id=id)
+    listing.delete()
+    return redirect("listings:list-listings")
